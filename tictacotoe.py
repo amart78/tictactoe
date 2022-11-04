f6b99546f6afae29740a699a7a715608b34a8fe2
Alexis M <97429214+amart78@users.noreply.github.com>
2022-11-04 18:07:22 -0500

    Update tictacotoe.py
    Game now fully functional in the terminal
    Written with a goal to use a minimal amount of pre-built functions
    ver 1

--- tictacotoe.py
+++ tictacotoe.py
@@ -1,101 +1,60 @@
 from re import X
 from array import *
- 
-a = 'a'
-b = 'b'
-c = 'c'
-d = 'd'
-e = 'e'
-f = 'f'
-g = 'g'
-h = 'h'
-i = 'i'
- 
-# 2D arrary to represent tictactoe game board
-ttt_board = [
-            [a, b, c],
-            [d ,e ,f],
-            [g, h, i]
-                    ]
- 
-# Find dimension/size of board (traditionally 3x3 but added to accommodate other sizes)
-board_size = len(ttt_board[0])
-# print(board_size)
- 
-# Identify row 'coordinates'
-x = 0
-y = 0
- 
-while x < board_size:
-    while y < board_size :
-        # print(x, y)
-        y+=1
-    y = 0
-    x+= 1
- 
-# Identify column 'coordinates'
-x = 0
-y = 0
- 
-while y < board_size:
-    while x < board_size:
-        # print(x, y)
-        x+=1
-    x = 0
-    y+= 1
- 
-x = 0
-y = 0
- 
-# Identify L to R diagonal 'coordinates'
-while x < board_size:
-    while y < board_size:
-        # print(x, y)
-        x+= 1
-        y+= 1
- 
-# Identify R to L diagonal 'coordinates'
-x = 0
-y = 2
- 
-while x < board_size:
-    while y >= 0:
-        # print(x, y)
-        x+= 1
-        y-= 1
 
-  
 def game():
-    # Game intro & starting board presentation
-    print("Let's play tic tac toe! Each position on the board is represented by a letter.")
-    print("Please review the game board below & choose your position accordingly. Player X is first!")
-    
-    for row in ttt_board:
-        for val in row:
-            print(val, end=" ")
-        print()
+    while True:
+        # 2D arrary to represent tictactoe game board
+        ttt_board = [
+                     ['a', 'b', 'c'],
+                     ['d', 'e', 'f'],
+                     ['g', 'h', 'i']
+                    ]
+        # Find dimension/size of board (traditionally 3x3 but added to accommodate other sizes)
+        board_size = len(ttt_board[0])
+       
+        # Game intro & starting board presentation
+        print("Let's play tic tac toe! Each position on the board is represented by a letter.")
+        print("Please review the game board below & choose your position accordingly. Player X is first!")
+        
+        for row in ttt_board:
+            for val in row:
+                print(val, end=" ")
+            print()
 
-# default starting player is X
-    player = 'X'
-    
-# while there are open positions, continue game
-    not_all_x_o = True
+        # default starting player is X
+        player = 'X'
+        
+        # while there are open positions, continue game
+        not_all_x_o = True
+        no_winning_combo = True
 
-    while not_all_x_o:
+        while not_all_x_o == True and no_winning_combo == True:
 
-    # player picks the letter associated with the spot they'd like to choose
-        position = input(f"Player {player}, choose your position: ")
-    
-        if position == 'O' or position == 'X' or position == None:
-            print('Invalid option inputted. Try again.')
-        else:
-    # index/coordinates of the chosen letter are found
+            # player picks the letter associated with the spot they'd like to choose
+            position = input(f"Player {player}, choose your position: ")
+            
+            # check if chosen position is valid & available
+            if position == 'O' or position == 'X' or position == None or position == '':
+                print('Invalid option inputted. Try again.')
+                continue
+            
+            valid = False
+            for row in ttt_board:
+                for val in row:
+                    if position == val:
+                        valid = True
+
+            if not valid:
+                print('Invalid option inputted. Try again.')
+                continue
+
+        # index/coordinates of the chosen letter are found
+        # else:
             for i, j in enumerate(ttt_board):
                 if position in j:
                     index = (i,j.index(position))
                     break
-            print(f"{position} is located at {index}")
-
+            
         # update board accordingly using the coordinates of the letter found above
             ttt_board[index[0]][index[1]] = player
 
@@ -106,8 +65,102 @@ def game():
                 print()
 
         # check game status
+            # check for 3 in a row aka winning combo
+            x = 0
+            y = 0
+
+            last_val = ''
+            no_winning_combo = True
+
+            # horizontally
+            while x < board_size:
+                match_count = 0
+
+                while y < board_size :
+
+                    if ttt_board[x][y] == last_val:
+                        match_count += 1
+
+                    if match_count == 2:
+                        no_winning_combo = False
+                        print(f"Player {player} wins!")
+
+                    last_val = ttt_board[x][y]
+                    y+=1
+
+                y = 0
+                x+= 1
+            
+            # vertically
+            x = 0
+            y = 0
+            last_val = ''
+
+            while y < board_size:
+                match_count = 0
+
+                while x < board_size:
+
+                    if ttt_board[x][y] == 'O' or ttt_board[x][y] == 'X':
+                        if ttt_board[x][y] == last_val:
+                            match_count += 1
+                    
+                    if match_count == 2:
+                        no_winning_combo = False
+                        print(f"Player {player} wins!")
+
+                    last_val = ttt_board[x][y]
+                    x+=1
+
+                x = 0
+                y+= 1
+
+            # diagonally L to R
+            x = 0
+            y = 0
+            last_val = ''
+
+            while x < board_size:
+                match_count = 0
+
+                while y < board_size:
+                    
+                    if ttt_board[x][y] == 'O' or ttt_board[x][y] == 'X':
+                        if ttt_board[x][y] == last_val:
+                            match_count += 1
+
+                    if match_count == 2:
+                        no_winning_combo = False
+                        print(f"Player {player} wins!")
+
+                    last_val = ttt_board[x][y]
+                    x+= 1
+                    y+= 1
+            
+            # diagonally R to L
+            x = 0
+            y = board_size - 1      # =2
+            last_val = ''
+
+            while x < board_size:
+                match_count = 0
+
+                while y >= 0:
+
+                    if ttt_board[x][y] == 'O' or ttt_board[x][y] == 'X':
+                        if ttt_board[x][y] == last_val:
+                            match_count += 1
+
+                    if match_count == 2:
+                        no_winning_combo = False
+                        print(f"Player {player} wins!")
+
+                    last_val = ttt_board[x][y]
+                    x+= 1
+                    y-= 1
+                    
+            # check for full game board
             not_all_x_o = False
-            winning_combo = False
 
             for list in ttt_board:
                 for val in list:
@@ -122,7 +175,5 @@ def game():
                 player = "O"
             else:
                 player = "X"
- 
-# else (if winning combo IS present), the last player who did their turn wins. Reset game
  
 game()
